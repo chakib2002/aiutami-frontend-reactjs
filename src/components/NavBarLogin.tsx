f7372f2554ddcbd9d0114b0a0e2cf8bba000833a
chakib2002 <Abdelmalek.feddal@worldlearningalgeria.org>
2022-04-06 16:17:31 +0100

    modification

--- src/components/NavBarLogin.tsx
+++ src/components/NavBarLogin.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 export const NavBarLogin = () => {
   return (
-    <div className='bg-primary py-5'>
-        <Link to= '/'>
-          <p className=' w-48 cursor-pointer font-bold text-2xl text-secondary px-5 active:text-bluedark transition transition-duration-400 ease-out '>Aiutami</p>
-        </Link>
-      </div>
-  )
-}
+    <div className="bg-primary py-5">
+      <Link to="/">
+        <p className="text-left w-48 cursor-pointer font-bold text-2xl text-secondary px-5 active:text-bluedark transition transition-duration-400 ease-out ">
+          Aiutami
+        </p>
+      </Link>
+    </div>
+  );
+};
